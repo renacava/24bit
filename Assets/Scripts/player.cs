d3de867cbf38db2af17ce878e57481a7ef3669c9
renacava <renanocava@gmail.com>
2025-02-10 19:40:38 +0200

    add tilemap; move player.cs; attach cam to player

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float movementSpeed = 10;
+    float speedX, speedY;
+    Rigidbody2D playerRigidBody;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerRigidBody.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    void movePlayer(){
+        speedX = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movementSpeed;
+        playerRigidBody.linearVelocity = new Vector2(speedX, speedY);
+    }
+
+}
