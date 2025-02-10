c84d68e3e8b8e6fb9f718a349c6cc28da9fea17d
renacava <renanocava@gmail.com>
2025-02-10 22:03:44 +0200

    refactor Player.cs; player and enemy prefabs use appropriate tags

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,35 +9,37 @@ public class Player : MonoBehaviour
     public float health = 100;
     Rigidbody2D playerRigidBody;
     List<ContactPoint2D> contacts = new List<ContactPoint2D>();
+    List<GameObject> touchedGameObjects = new List<GameObject>();
+    bool isTouchingEnemy = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
-        playerRigidBody.gravityScale = 0;
+        InitialiseRigidBody();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //TakeDamage();
         ProcessDamage();
         MovePlayer();
-        Director.UpdatePlayerPosition((Vector2)transform.position);
+    }
+
+    void InitialiseRigidBody(){
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerRigidBody.gravityScale = 0;
     }
 
     void MovePlayer(){
         speedX = Input.GetAxisRaw("Horizontal") * movementSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movementSpeed;
         playerRigidBody.linearVelocity = new Vector2(speedX, speedY);
+        Director.UpdatePlayerPosition((Vector2)transform.position);
     }
 
-
-
     void ProcessDamage(){
-        
-        playerRigidBody.GetContacts(contacts);
-        if (contacts.Count > 0)
+        UpdateIsTouchingEnemy();
+        if (isTouchingEnemy)
             TakeDamage();
         if (health <= 0)
             Director.RestartScene();
@@ -47,6 +49,24 @@ public class Player : MonoBehaviour
         health -= 0.5f;
     }
 
+    void UpdateIsTouchingEnemy(){
+        UpdateTouchedThings();
+        isTouchingEnemy = false;
+        foreach (GameObject touchedGameObject in touchedGameObjects){
+            if (touchedGameObject.tag == "Enemy"){
+                isTouchingEnemy = true;
+                break;
+            }
+        }
+    }
+
+    void UpdateTouchedThings(){
+        playerRigidBody.GetContacts(contacts);
+        touchedGameObjects.Clear();
+        foreach (ContactPoint2D contact in contacts){
+            touchedGameObjects.Add(contact.collider.gameObject);
+        }
+    }
 
 
 }
